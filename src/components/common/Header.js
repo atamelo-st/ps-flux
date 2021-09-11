0300853c12130c85d580123a390d141656078f51
Andrey Tamelo <atamelo@servicetitan.com>
2021-09-10 19:35:19 -0700

    adding courses Table

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -5,7 +5,7 @@ function Header() {
     return (
 
         <nav>
-            <a href="/">Home</a> | <a href="/about">About</a>
+            <a href="/">Home</a> | <a href="/courses">Courses</a> | <a href="/about">About</a>
         </nav>
     );
 }
